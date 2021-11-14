e4735298c3eee1154dec9b3e64f50d3f1da793ff
Ray Milkey <ray@opennetworking.org>
2021-11-14 14:57:43 -0800

    Add common makefile

---
+++ make/onf-common.mk
@@ -0,0 +1,31 @@
+deps: # @HELP ensure that the required dependencies are in place
+	go build -v ./...
+	bash -c "diff -u <(echo -n) <(git diff go.mod)"
+	bash -c "diff -u <(echo -n) <(git diff go.sum)"
+
+linters: golang-ci # @HELP examines Go source code and reports coding problems
+	golangci-lint run --timeout 5m
+
+gofmt: # @HELP run the Go format validation
+	bash -c "diff -u <(echo -n) <(gofmt -d pkg/ cmd/ tests/)"
+
+jenkins-tools: # @HELP installs tooling needed for Jenkins
+	cd .. && go get -u github.com/jstemmer/go-junit-report && go get github.com/t-yuki/gocover-cobertura
+
+golang-ci: # @HELP install golang-ci if not present
+	golangci-lint --version || curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.42.1
+
+license_check: # @HELP examine and ensure license headers exist
+	@if [ ! -d "../build-tools" ]; then cd .. && git clone https://github.com/onosproject/build-tools.git; fi
+	./../build-tools/licensing/boilerplate.py -v --rootdir=${CURDIR}
+
+bumponosdeps: # @HELP update "onosproject" go dependencies and push patch to git.
+	./../build-tools/bump-onos-deps ${VERSION}
+
+help:
+	@grep -E '^.*: *# *@HELP' $(MAKEFILE_LIST) \
+    | sort \
+    | awk ' \
+        BEGIN {FS = ": *# *@HELP"}; \
+        {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}; \
+    '
