98e06561e9572cf1b2778c2cf3a2f228dc442a31
Ray Milkey <ray@opennetworking.org>
2021-11-23 09:04:03 -0800

    Add target for member license check, int test namespace

--- make/onf-common.mk
+++ make/onf-common.mk
@@ -19,11 +19,16 @@ golang-ci: # @HELP install golang-ci if not present
 	golangci-lint --version || curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b `go env GOPATH`/bin v1.42.1
 
 license_check: # @HELP examine and ensure license headers exist
-	@if [ ! -d "../build-tools" ]; then cd .. && git clone https://github.com/onosproject/build-tools.git; fi
-	./../build-tools/licensing/boilerplate.py -v --rootdir=${CURDIR}
+	./build/build-tools/licensing/boilerplate.py -v --skipped-dir build --rootdir=${CURDIR}
+
+license_check_member_only: # @HELP examine and ensure license headers exist for member only license
+	./build/build-tools/licensing/boilerplate.py -v --skipped-dir build --rootdir=${CURDIR} --boilerplate LicenseRef-ONF-Member-Only-1.0
 
 bumponosdeps: # @HELP update "onosproject" go dependencies and push patch to git.
-	./../build-tools/bump-onos-deps ${VERSION}
+	./build/build-tools/bump-onos-deps ${VERSION}
+
+integration-test-namespace:
+	(kubectl delete ns test || exit 0) && kubectl create ns test
 
 clean:: # @HELP cleans the downloaded build tools directory
 	rm -rf ./build/build-tools
