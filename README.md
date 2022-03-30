307afa888138e2fd073d103b9a7633a4fc6af65b
Ray Milkey <ray.milkey@gmail.com>
2022-03-30 11:21:15 -0700

    Remove old licensing tool, plust Travis and Coveralls tools

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ This repository contains build tools used across many repositories.
 
 Tools:
 
-- [license checker](./licensing)
 - [common make targets](./make)
--- make/onf-common.mk
+++ make/onf-common.mk
@@ -32,15 +32,6 @@ reuse-tool: # @HELP install reuse if not present
 license: reuse-tool # @HELP run license checks
 	reuse lint
 
-license_check: # @HELP examine and ensure license headers exist
-	./build/build-tools/licensing/boilerplate.py -v --skipped-dir build --rootdir=${CURDIR}
-
-license_check_member_only: # @HELP examine and ensure license headers exist for member only license
-	./build/build-tools/licensing/boilerplate.py -v --skipped-dir build --rootdir=${CURDIR} --boilerplate LicenseRef-ONF-Member-Only-1.0
-
-license_check_apache: # @HELP examine and ensure license headers exist for the Apache 2.0 license
-	./build/build-tools/licensing/boilerplate.py -v --skipped-dir build --rootdir=${CURDIR} --boilerplate SPDX-Apache-2.0
-
 bumponosdeps: # @HELP update "onosproject" go dependencies and push patch to git.
 	./build/build-tools/bump-onos-deps ${VERSION}
 
